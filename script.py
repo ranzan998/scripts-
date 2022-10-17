ca5c490acd667b539b781e7fabc9c8eb27969345
Ranzan_998 <88772203+ranzan998@users.noreply.github.com>
2022-10-17 15:00:46 +0530

    Update script.py

--- script.py
+++ script.py
@@ -6,5 +6,11 @@ import os
 # test.jnl is a ferret script that we want to run from python itself.
 os.system('conda run -n FERRET ferret -script wavelet_cw_ccw.jnl')
 
-#
-os.system(' ')
+# To run a fortran code
+os.system('gfortran test.f -o test.o')
+
+#to run a fortran code that has netcdf dependency, and needs to be linked with other subroutines inside the code
+os.system('f77 -c -I/usr/include test.f; f77 -o test test.o -L/{path to netcdff} -lnetcdff; ./test')
+
+# multiple codes like this can be stored in a shell script (e.g, test_multiple.sh) and can be called through python
+os.system('bash test_multiple.sh')
